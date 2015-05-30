7dcf6143dda9d3adbd75505a47f0f612aa74425c
misaki-nyanya <Jbrass5540226@163.com>
2015-05-30 11:56:46 +0900

    Create CommunicationException.java

---
+++ TASystem/src/theBigHomework/CommunicationException.java
@@ -0,0 +1,12 @@
+package theBigHomework;
+
+public class CommunicationException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2849411255094811910L;
+	public CommunicationException(){}
+	public CommunicationException(String msg){
+		super(msg);
+	}
+}
