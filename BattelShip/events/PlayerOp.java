10cf4a30ece0f8a598698b9fc577f80c0e2418ea
misaki-nyanya <Jbrass5540226@163.com>
2015-05-30 12:10:57 +0900

    Create PlayerOp.java

---
+++ BattelShip/events/PlayerOp.java
@@ -0,0 +1,16 @@
+package events;
+
+import client.Coordinate;
+import zjs.smartevents.events.Event;
+
+public class PlayerOp extends Event{
+	private static final long serialVersionUID = 1L;
+	public Coordinate[] op;
+	public int[] op_motion;
+	public int playerid;
+	public int playerLeftShip;
+	public static void main(String[] args) {
+
+	}
+
+}
